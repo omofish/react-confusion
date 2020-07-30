9d9e7d6aec22182826f34f1b9b47102a0e70e644
omofish <jason_lzx@hotmail.com>
2020-07-30 14:49:47 +0800

    Components Part 1

---
+++ src/components/component template.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class foo extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+
+    );
+  }
+}
+
+export default foo
